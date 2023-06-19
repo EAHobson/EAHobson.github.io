116776c0cae8fbdc6b7e1272f3f9ecd6c886e07e
Joshua Garland <garland.joshua@gmail.com>
2023-06-19 09:33:58 -0700

    Fixed diagonal movement and player speed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const MAPWIDTH = 50; // width of the map (in tiles)
 const MAPHEIGHT = 50; // height of the map (in tiles)
 const TIMESPEED = 5; // the speed that time passes in the game (amount of time units from csv per second)
 const CHARACTER_SIZE = 3; // the size of the characters (in tiles)
-const PLAYER_SPEED = 3; // the speed of the player (tiles per second)
+const PLAYER_SPEED = 1; // the speed of the player (tiles per second)
 const PLAYER_ID = -1; // the id the player will have in the final data sheet (in the agent column)
 const TAG_COLOR = "black" // the color of the tag above the characters
 
@@ -91,18 +91,23 @@ class Player extends Character {
         this.speed = PLAYER_SPEED;
     }
     update(timePassed, totaltime) {
+        /*
+        WARNING! DIAGONAL FIX: The below if elseif was modified to remove diagonal mvmt. 
+        If the "elses" below are removed and made into individual if statements then
+        diagonal movement will be allowed again.
+        */
         if (keys["ArrowRight"]) {
             this.x += this.speed * timePassed;
             this.spriteIndex = 0;
         }
-        if (keys["ArrowLeft"]) {
+        else if (keys["ArrowLeft"]) {
             this.x -= this.speed * timePassed;
             this.spriteIndex = 1;
         }
-        if (keys["ArrowUp"]) {
+        else if (keys["ArrowUp"]) {
             this.y -= this.speed * timePassed;
         }
-        if (keys["ArrowDown"]) {
+        else if (keys["ArrowDown"]) {
             this.y += this.speed * timePassed;
         }
         if(this.x < 0) {
